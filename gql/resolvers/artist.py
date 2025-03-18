5762aa05ecd59426102f6055a10bc55718b2d972
Yuri Fontella <yurifc4@gmail.com>
2025-03-17 21:40:42 -0300

    sessionmaker

--- gql/resolvers/artist.py
+++ gql/resolvers/artist.py
@@ -2,24 +2,22 @@ import typing
 
 import gql.types.model as model
 
+from sqlalchemy import select, column, insert
 from gql.types.input import ArtistInput
 
-from sqlalchemy.orm import Session
-from sqlalchemy import select, column, insert
-
-from orm.engine import engine
 from orm.tables import artists
+from orm.session import get_session
 
 
 def get_artists() -> typing.List["model.Artist"]:
-    with Session(engine) as session:
+    with get_session() as session:
         records = session.execute(select(artists))
 
     return [model.Artist(id=row.id, name=row.name) for row in records]
 
 
 def post_artist(data: ArtistInput) -> "model.Artist":
-    with Session(engine) as session:
+    with get_session() as session:
         artist = insert(artists).values(name=data.name).returning(column('id'))
         result = session.execute(artist).scalar()
         session.commit()
@@ -28,7 +26,7 @@ def post_artist(data: ArtistInput) -> "model.Artist":
 
 
 def get_artist_by_id(artist_id) -> "model.Artist":
-    with Session(engine) as session:
+    with get_session() as session:
         record = session.execute(select(artists).where(column('id') == artist_id)).one()
 
     return model.Artist(id=record.id, name=record.name)
--- gql/resolvers/music.py
+++ gql/resolvers/music.py
@@ -2,22 +2,21 @@ import typing
 
 import gql.types.model as model
 
-from sqlalchemy.orm import Session
 from sqlalchemy import select, column
 
 from orm.tables import musics
-from orm.engine import engine
+from orm.session import get_session
 
 
 def get_musics() -> typing.List["model.Music"]:
-    with Session(engine) as session:
+    with get_session() as session:
         records = session.execute(select(musics))
 
     return [model.Music(id=row.id, title=row.title, artist_id=row.artist_id) for row in records]
 
 
 def get_musics_by_artist(artist_id) -> typing.List["model.Music"]:
-    with Session(engine) as session:
+    with get_session() as session:
         records = session.execute(select(musics).where(column('artist_id') == artist_id))
 
     return [model.Music(id=row.id, title=row.title, artist_id=row.artist_id) for row in records]
