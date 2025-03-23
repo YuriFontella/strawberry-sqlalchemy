d9060e56aff0006c7f265cfa3f389f699c3c4fb6
Yuri Fontella <yurifc4@gmail.com>
2025-03-22 23:27:25 -0300

    import base

--- gql/resolvers/artist.py
+++ gql/resolvers/artist.py
@@ -1,7 +1,4 @@
-from typing import List, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from gql.types.base import Artist
+from typing import List
 
 from sqlalchemy import select, column, insert
 from gql.types.input import ArtistInput
@@ -9,24 +6,26 @@ from gql.types.input import ArtistInput
 from orm.tables import artists
 from orm.session import get_session
 
+import gql.types.base as base
 
-def get_artists() -> List["Artist"]:
+
+def get_artists() -> List["base.Artist"]:
     with get_session() as session:
         records = session.execute(select(artists))
 
-    return [Artist(id=row.id, name=row.name) for row in records]
+    return [base.Artist(id=row.id, name=row.name) for row in records]
 
 
-def post_artist(data: ArtistInput) -> "Artist":
+def post_artist(data: ArtistInput) -> "base.Artist":
     with get_session() as session:
         artist = insert(artists).values(name=data.name).returning(column('id'))
         result = session.execute(artist).scalar()
 
-    return Artist(id=result, name=data.name)
+    return base.Artist(id=result, name=data.name)
 
 
-def get_artist_by_id(artist_id) -> "Artist":
+def get_artist_by_id(artist_id) -> "base.Artist":
     with get_session() as session:
         record = session.execute(select(artists).where(column('id') == artist_id)).one()
 
-    return Artist(id=record.id, name=record.name)
+    return base.Artist(id=record.id, name=record.name)
--- gql/resolvers/music.py
+++ gql/resolvers/music.py
@@ -1,7 +1,4 @@
-from typing import List, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from gql.types.base import Music
+from typing import List
 
 from sqlalchemy import select, column, insert
 from gql.types.input import MusicInput
@@ -9,24 +6,26 @@ from gql.types.input import MusicInput
 from orm.tables import musics
 from orm.session import get_session
 
+import gql.types.base as base
 
-def get_musics() -> List["Music"]:
+
+def get_musics() -> List["base.Music"]:
     with get_session() as session:
         records = session.execute(select(musics))
 
-    return [Music(id=row.id, title=row.title, artist_id=row.artist_id) for row in records]
+    return [base.Music(id=row.id, title=row.title, artist_id=row.artist_id) for row in records]
 
 
-def post_music(data: MusicInput) -> "Music":
+def post_music(data: MusicInput) -> "base.Music":
     with get_session() as session:
         music = insert(musics).values(title=data.title, artist_id=data.artist_id).returning(column('id'), column('artist_id'))
         result = session.execute(music).one()
 
-    return Music(id=result.id, title=data.title, artist_id=result.artist_id)
+    return base.Music(id=result.id, title=data.title, artist_id=result.artist_id)
 
 
-def get_musics_by_artist(artist_id) -> List["Music"]:
+def get_musics_by_artist(artist_id) -> List["base.Music"]:
     with get_session() as session:
         records = session.execute(select(musics).where(column('artist_id') == artist_id))
 
-    return [Music(id=row.id, title=row.title, artist_id=row.artist_id) for row in records]
+    return [base.Music(id=row.id, title=row.title, artist_id=row.artist_id) for row in records]
