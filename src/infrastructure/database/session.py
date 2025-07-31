b30acfcb4de7181f2d5527f586f082496cf89cd0
Yuri Fontella <yurifc4@gmail.com>
2025-07-31 19:48:12 -0300

    feat: add complete configuration pool

--- src/infrastructure/database/session.py
+++ src/infrastructure/database/session.py
@@ -3,12 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import metadata
 from src.infrastructure.config.settings import get_database_url
+from sqlalchemy.pool import QueuePool
+
 
 # Criar engine do banco de dados
-engine = create_engine(get_database_url())
+engine = create_engine(
+    url=get_database_url(),
+    echo = False,
+    poolclass = QueuePool,
+    pool_pre_ping = True,
+    pool_recycle = 1800,
+    pool_timeout = 30,
+    pool_size = 10,
+    max_overflow = 20
+)
 
 # Criar session factory
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, expire_on_commit=False, bind=engine)
 
 
 @contextmanager
